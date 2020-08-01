9c377f8efad88b80564dd4b25e476e8c4067e429
Arash Ghamkhari <arashghamkhari@gmail.com>
2020-08-01 23:11:22 +0430

    Fix a bug

--- rwmysql.d.ts
+++ rwmysql.d.ts
@@ -1,8 +1,6 @@
 import {Pool, PoolConnection} from "mysql";
 
-interface RWMysql {
-  constructor(pool: Pool, reconnect: number | null, queryRetry: number | null)
-
+interface rwmysql {
   read(sql: string, params: Array<any>): Promise<Array<Object>>
 
   write(sql: string, params: Array<any>): Promise<{ effectedRows: number, changedRows: number, insertId: number }>
@@ -11,5 +9,7 @@ interface RWMysql {
 
   getConnection(): Promise<PoolConnection>
 }
+
+declare function RWMysql(pool: Pool, reconnect: number | null, queryRetry: number | null): rwmysql
 
 export = RWMysql
